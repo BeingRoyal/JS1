34fe8c505a6db13223abb1dceda0dcb17ef8749d
Hussain Shahid <111196484+BeingRoyal@users.noreply.github.com>
2024-07-25 16:33:36 +0000

    container config done

---
+++ basics/veriables.js
@@ -0,0 +1,7 @@
+const accountId = 324978
+var accountEmail = 'hussain.gamil'
+let accountPassword = '985739'
+city = "sialkot"
+let veriableState
+
+console.table([accountId, accountEmail, accountPassword, veriableState])
